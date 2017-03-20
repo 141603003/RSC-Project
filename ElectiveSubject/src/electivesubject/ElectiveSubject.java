a4d689b5239a5fc1812459275d25148c16aa5b81
Vinay Desai <desaivinay1997@gmail.com>
2017-03-21 00:32:53 +0530

    GUI

---
+++ ElectiveSubject/src/electivesubject/ElectiveSubject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electivesubject;
+
+/**
+ *
+ * @author vinay
+ */
+public class ElectiveSubject {
+    public static MainMenuForm form;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        form = new MainMenuForm();
+        form.setVisible(true);
+    }
+    
+}
